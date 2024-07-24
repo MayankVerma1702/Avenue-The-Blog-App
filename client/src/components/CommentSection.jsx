ae76d38334b6ba33acd0bed7efc2456b17126ab6
Mayank <mayankdeo1702@gmail.com>
2024-07-25 02:09:13 +0530

    Add like functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,7 +1,7 @@
-import { Alert, Button, Textarea, TextInput } from "flowbite-react";
+import { Alert, Button, Textarea } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Comment from "./Comment";
 
 function CommentSection({ postId }) {
@@ -9,6 +9,7 @@ function CommentSection({ postId }) {
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
   const [commentError, setCommentError] = useState(null);
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -19,7 +20,7 @@ function CommentSection({ postId }) {
       const res = await fetch(`/api/comment/create`, {
         method: "POST",
         headers: {
-          "COntent-Type": "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           content: comment,
@@ -52,7 +53,35 @@ function CommentSection({ postId }) {
       }
     };
     getComments();
-  }, [postId]);
+  }, [postId, comment.likes]);
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
@@ -119,9 +148,15 @@ function CommentSection({ postId }) {
               <p> {comments.length} </p>
             </div>
           </div>
-          {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
-          ))}
+          {comments.map((comment) =>
+            comment && comment._id ? (
+              <Comment
+                key={comment._id}
+                comment={comment}
+                onLike={handleLike}
+              />
+            ) : null
+          )}
         </>
       )}
     </div>
