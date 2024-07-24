dd6d2388b58915ec6816143b7bd0a3a449094960
Mayank <mayankdeo1702@gmail.com>
2024-07-24 13:15:14 +0530

    Add refernce to call to action

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CallToAction() {
+  return (
+    <div>
+      <div className="">CallToAction</div>
+      <div className="">{/* <img src="" alt="" /> */}</div>
+    </div>
+  );
+}
+
+export default CallToAction;
