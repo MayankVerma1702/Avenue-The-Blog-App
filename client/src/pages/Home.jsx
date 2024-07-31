186ca76c5461bdb345d023499bf2d4bcc9cedd65
Mayank <mayankdeo1702@gmail.com>
2024-07-31 12:50:45 +0530

    complete the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import PostCard from "../components/PostCard";
 
 function Home() {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/post/getPosts");
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome To My Blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          Welcome to my blog, where curiosity meets creativity! Dive into a
+          world of technology with in-depth articles on the latest innovations,
+          trends, and how they shape our future. Explore the realms of fiction
+          and mystery, where captivating stories and intriguing plots await,
+          designed to ignite your imagination and keep you on the edge of your
+          seat. Whether you're a tech enthusiast, a lover of compelling
+          narratives, or someone who enjoys unraveling mysteries, there's
+          something here for everyone. Join me on this exciting journey and
+          let's discover the wonders of technology and storytelling together!
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className=" flex flex-wrap gap-2">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
