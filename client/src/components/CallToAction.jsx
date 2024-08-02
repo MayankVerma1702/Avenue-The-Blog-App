003016e2bf48c04513e833190db9e9e1c2930ec8
Mayank <mayankdeo1702@gmail.com>
2024-08-02 17:25:55 +0530

    Update call to action

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import { Button } from "flowbite-react";
 
-function CallToAction() {
+export default function CallToAction() {
   return (
-    <div>
-      <div className="">CallToAction</div>
-      <div className="">{/* <img src="" alt="" /> */}</div>
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">Try my File Transfer Application(FisFr)!</h2>
+        <p className="text-gray-500 my-2">
+          This appliation is made purely in vanillaJS
+        </p>
+        <Button
+          gradientDuoTone="purpleToPink"
+          className="rounded-tl-xl rounded-bl-none"
+        >
+          <a
+            href="https://fisfr-frontend.onrender.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            FisFr
+          </a>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img src="../undraw-upload.svg" />
+      </div>
     </div>
   );
 }
-
-export default CallToAction;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -36,7 +36,7 @@ function Home() {
           View all posts
         </Link>
       </div>
-      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+      <div className="p-3 bg-indigo-200 dark:bg-slate-700">
         <CallToAction />
       </div>
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
